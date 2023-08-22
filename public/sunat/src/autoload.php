33a8b736e93bc90fe57add746bf04a14469924a0
veramindi <peruu555peru@gmail.com>
2023-08-22 12:14:58 -0500

    nuevosistema

--- public/sunat/src/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once(__DIR__ . "/obj.php");
-	require_once(__DIR__ . "/curl.php");
-	require_once(__DIR__ . "/sunat.php");
-?>
--- vistas/prueba.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$mesN = 3;
-$mes = str_pad((string)$mesN,2,"0",STR_PAD_LEFT);
-echo $mes;
- ?>
